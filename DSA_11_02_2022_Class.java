b4f0b8646a6bba8bdb25424c40309a2c66845395
Amit-rajput97 <84862054+Amit-rajput97@users.noreply.github.com>
2022-02-12 21:24:15 +0530

    Update DSA_11_02_2022_Class.java

--- DSA_11_02_2022_Class.java
+++ DSA_11_02_2022_Class.java
@@ -5,17 +5,18 @@ package LinkedLists;
   Convert each element of argumented string in its code form which is as follows:
   if number is Positive then Starts with : "P" (As String)
   Else starts with : "N";
-  
+
   Also check each digit of number is even or not,
   if digit is Even than add "e" in the previous string,
   else add "o" in the existing String.
-  Add this newly formed code of String in the Linked List and 
+  Add this newly formed code of String in the Linked List and
   Display the output of given in a coded form.
-  
+
   Input: {"12345","-67823","00123"...}
   Output: "Poeoeo","Neoeeo","Poeo" (Ignore 0s in the Starting in a Number)
-  
+
  */
+
 class Node11
 {
     String data;
@@ -37,7 +38,7 @@ public class DSA_11_02_2022_Class {
             if (temp>0)                              //---------If Number is Positive
             {
                 res = "P";                             //---------For +ve ,we use -P
-                String str = args[i];                 //---------Storing args at ith index in a temporary string
+                String str = Integer.toString(temp);                 //---------Storing args at ith index in a temporary string
                 for (int j=0; j<str.length(); j++)         //----------using loop so that we can check each digit
                 {
                     if (str.charAt(j)%2 ==0)              //-----------If digit is even
@@ -78,13 +79,29 @@ public class DSA_11_02_2022_Class {
     static Node11 head;
     public static void insert(String res)
     {
-        //head = null;
+//        head = null;
         //----------CREATING A NEW NODE SO THAT IT ALLOCATE MEMORY
         Node11 newnode = new Node11();
 
+       //---------------1st way to insert values
         newnode.data = res;                //---------ASSIGNING VALUES TO THE NEW NODE
         newnode.next = head;                 //---------IT HELPS TO NEW NODE TO EXISTING NODE
         head = newnode;                      //---------UPDATE TOP REFERENCE
+
+
+
+//         ----------------2nd way to insert values
+//        if (head==null)
+//        {
+//            head = newnode;
+//
+//        }
+//        Node11 temp = head;
+//        while (temp.next!=null)
+//        {
+//            temp = temp.next;
+//            temp.next = newnode;
+//        }
     }
     public static void display()
     {
